aadda92f66d4320933ed408b822e67755c58c040
Echo <aliomaramerica@gmail.com>
2021-02-22 21:52:50 +0600

    Stage 4 Session Practice

---
+++ src/main/java/ProfileServlet.java
@@ -0,0 +1,31 @@
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+public class ProfileServlet extends HttpServlet {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html");
+        PrintWriter out=response.getWriter();
+        request.getRequestDispatcher("link.html").include(request, response);
+
+        HttpSession session=request.getSession(false);
+        if(session!=null){
+            String name=(String)session.getAttribute("name");
+
+            long createdTime = session.getCreationTime();
+            long lastTime = session.getLastAccessedTime();
+            out.print("Hello, "+name+" Welcome to Profile");
+            out.println("Created time: " + createdTime);
+            out.println("Last access time: " + lastTime);
+        }
+        else{
+            out.print("Please login first");
+            request.getRequestDispatcher("login.html").include(request, response);
+        }
+        out.close();
+    }
+}
